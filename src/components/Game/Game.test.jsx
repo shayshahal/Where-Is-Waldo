3ed70eb28280d3a2d3c082f8817395228d9acea0
shayshahal <shay840@gmail.com>
2023-03-26 08:39:46 +0300

    fix game test (for now)

--- src/components/Game/Game.test.jsx
+++ src/components/Game/Game.test.jsx
@@ -1,6 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event/';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
+import { vi } from 'vitest';
+
 import Game from './Game';
 
 describe('Game tests', () => {
@@ -15,20 +17,22 @@ describe('Game tests', () => {
 		expect(screen.queryByTestId('easy-button')).toBeInTheDocument();
 		expect(screen.queryByTestId('hard-button')).toBeInTheDocument();
 	});
+
 	test('should start game on button click', async () => {
-		const user = userEvent.setup();
 		render(
 			<MemoryRouter>
 				<Game />
 			</MemoryRouter>
 		);
+		const user = userEvent.setup();
 
-		await user.click(expect(screen.queryByTestId('easy-button')));
+		await user.click(screen.getByTestId('easy-button'));
 
 		expect(screen.queryByRole('heading')).not.toBeInTheDocument();
 		expect(screen.queryByTestId('easy-button')).not.toBeInTheDocument();
 		expect(screen.queryByTestId('hard-button')).not.toBeInTheDocument();
 	});
+
 	test('should start game on button click', async () => {
 		const user = userEvent.setup();
 		render(
@@ -37,10 +41,31 @@ describe('Game tests', () => {
 			</MemoryRouter>
 		);
 
-		await user.click(expect(screen.queryByTestId('hard-button')));
+		await user.click(screen.getByTestId('easy-button'));
 
 		expect(screen.queryByRole('heading')).not.toBeInTheDocument();
 		expect(screen.queryByTestId('easy-button')).not.toBeInTheDocument();
 		expect(screen.queryByTestId('hard-button')).not.toBeInTheDocument();
+	});
+
+	test.todo('should increment score timer by 1 a second after game start', async () => {
+		const user = userEvent.setup();
+		render(
+			<MemoryRouter>
+				<Game />
+			</MemoryRouter>
+		);
+
+
+		await user.click(screen.getByTestId('easy-button'));
+		act(()=>{
+			vi.useFakeTimers();
+			vi.advanceTimersByTime(2000);
+		})
+
+
+
+
+		expect(screen.getByTestId('time')).toHaveTextContent('2');
 	});
 });
