a44b060e4fbafabf3c3f1c8112260bbae53cdf5f
shayshahal <shay840@gmail.com>
2023-03-26 08:41:39 +0300

    tests setup and such

--- src/components/Nav/Nav.test.tsx
+++ src/components/Nav/Nav.test.tsx
@@ -1,6 +1,6 @@
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
 import Nav from './Nav';
 
 describe('Nav tests', () => {
--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: 'AIzaSyCo5C6p3p_HPnEE0joxH1gnIv-3XAxLoKY',
-	authDomain: 'where-s-waldo-68512.firebaseapp.com',
-	projectId: 'where-s-waldo-68512',
-	storageBucket: 'where-s-waldo-68512.appspot.com',
-	messagingSenderId: '16399107379',
-	appId: '1:16399107379:web:8c7691081cb7f37b5baa46',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
