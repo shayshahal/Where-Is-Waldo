a8f5955e3e8180a2cda89c2f9bbe0f9c0ab87eaf
shayshahal <shay840@gmail.com>
2023-03-26 09:12:56 +0300

    add time formatting function

--- src/components/Game/Score/Score.tsx
+++ src/components/Game/Score/Score.tsx
@@ -7,10 +7,26 @@ interface scoreProps {
 }
 
 function Score({ time, found, toBeFound }: scoreProps) {
+	function toHHMMSS(time: number) {
+		let hours = ~~(time / 3600) as number | string;
+		let minutes = ~~(time / 60) as number | string;
+		let seconds = (time % 60) as number | string;
+		if (hours < 10) {
+			hours = '0' + hours;
+		}
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		return hours + ':' + minutes + ':' + seconds;
+	}
 
 	return (
 		<div className={styles.Score}>
-			<span data-testid='time'>{time}</span> <span>{found + '/' + toBeFound}</span>
+			<span data-testid='time'>{toHHMMSS(time)}</span>{' '}
+			<span>{found + '/' + toBeFound}</span>
 		</div>
 	);
 }
