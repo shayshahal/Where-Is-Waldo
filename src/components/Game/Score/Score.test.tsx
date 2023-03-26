673dbd63773f5e34ec4c13a72d5a1c986b641af1
shayshahal <shay840@gmail.com>
2023-03-26 08:40:00 +0300

    add time tests

--- src/components/Game/Score/Score.test.tsx
+++ src/components/Game/Score/Score.test.tsx
@@ -1,1 +1,21 @@
-import Score from './Score'
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Score from './Score';
+import { describe, expect, test } from 'vitest';
+
+describe('Score tests', () => {
+	test('should render score and amount to be found', () => {
+		render(
+			<MemoryRouter>
+				<Score
+					time={0}
+					found={2}
+					toBeFound={5}
+				/>
+			</MemoryRouter>
+		);
+
+		expect(screen.getByText('00:00:00')).toBeInTheDocument();
+		expect(screen.getByText('2/5')).toBeInTheDocument();
+	});
+});
