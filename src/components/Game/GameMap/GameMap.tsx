d2266c745b56bd06581b2bea0b01c4f02fd5ab2b
shayshahal <shay840@gmail.com>
2023-03-27 13:32:08 +0300

    finish tag logic

--- src/components/Game/GameMap/GameMap.tsx
+++ src/components/Game/GameMap/GameMap.tsx
@@ -1,10 +1,53 @@
 import { useState } from 'react';
 import styles from './GameMap.module.css';
+import Tag from './Tag/Tag';
 
-function GameMap() {
-	const [position, setPosition] = useState(0);
-	const [zoom, setZoom] = useState(0);
-	return <div className={styles.GameMap}></div>;
+interface GameMapProps {
+	onFound: (fnc: (n: number) => number) => void;
+	difficulty: 'easy' | 'hard';
+}
+
+interface Tag {
+	x: number;
+	y: number;
+	character: string;
+}
+
+interface Tags {
+	[key: string]: Tag;
+}
+
+function GameMap({ onFound, difficulty }: GameMapProps) {
+	const [tags, setTags] = useState<Tags>({});
+	function handleClick(e: React.MouseEvent<HTMLImageElement>) {
+		setTags({
+			...tags,
+			[e.pageX + '' + e.pageY]: {
+				x: e.pageX,
+				y: e.pageY,
+				character: 'Pandaman',
+			} as Tag,
+		});
+	}
+
+	return (
+		<div className={styles.GameMap}>
+			<img
+				className={styles.img}
+				alt='gameMap'
+				onClick={handleClick}
+				src='/src/assets/img.jpg'
+			></img>
+			{Object.entries(tags).map(([key, tag]) => (
+				<Tag
+					key={key}
+					x={tag.x}
+					y={tag.y}
+					character={tag.character}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default GameMap;
--- src/components/Game/GameMap/Tag/Tag.tsx
+++ src/components/Game/GameMap/Tag/Tag.tsx
@@ -1,10 +1,20 @@
-import styles from './Tag.module.css'
+import styles from './Tag.module.css';
 
-function Tag() {
-    return (
-        <div className={styles.Tag}>
-        </div>
-    )
+interface TagProps {
+	x: number;
+	y: number;
+	character: string;
 }
 
-export default Tag
+function Tag({ x, y, character }: TagProps) {
+	return (
+		<div
+			style={{ left: x + 'px', top: y + 'px' }}
+			className={styles.Tag}
+		>
+			{character}
+		</div>
+	);
+}
+
+export default Tag;
