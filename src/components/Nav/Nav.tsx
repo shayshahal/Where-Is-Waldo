3221a922ffb21831112acaf6c4db813483c44f15
shayshahal <shay840@gmail.com>
2023-03-27 13:47:39 +0300

    redesign nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,24 +3,12 @@ import { NavLink } from 'react-router-dom';
 import styles from './Nav.module.css';
 
 function Nav() {
-	const [isShowing, setIsShowing] = useState(false);
 	return (
 		<nav className={styles.Nav}>
-			{<button
-				onClick={() => {
-					setIsShowing((prev) => !prev);
-				}}
-				className={styles.button} 
-			>
-				☰
-			</button>}
-			{isShowing && (
-				<div data-testid='nav-links' className={styles.navLinks} >
-					<NavLink className={styles.navLink} to='/'>Game</NavLink>
-					<NavLink className={styles.navLink} to='/Leaderboards'>Leaderboards</NavLink>
-					<NavLink className={styles.navLink} to='/Info'>Info</NavLink>
-				</div>
-			)}
+			<NavLink className={styles.navLink} to='/'>Home</NavLink>
+			<NavLink className={styles.navLink} to='/Leaderboards'>Leaderboards</NavLink>
+			<NavLink className={styles.navLink} to='/Info'>Info</NavLink>
+		
 		</nav>
 	);
 }
