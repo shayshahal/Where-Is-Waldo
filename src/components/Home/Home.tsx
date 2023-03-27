9a5653a43f6dbc77669c1ba0a4831db4e4075899
shayshahal <shay840@gmail.com>
2023-03-27 17:20:13 +0300

    Redesign home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,25 +1,68 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Game from './Game/Game';
-import styles from './Home.module.css';
 import Score from './Game/Score/Score';
+import styles from './Home.module.css';
 
-type GameState = 'inactive' | 'easy' | 'hard';
+type gameChoice = 'official' | 'fanmade' | '';
 
 function Home() {
-	const [gameState, setGameState] = useState('inactive' as GameState);
-	const [gameTime, setGameTime] = useState(0);
-	const [amountFound, setAmountFound] = useState(0);
-	let timer: number;
+	const [gameChoice, setGameChoice] = useState<gameChoice>('');
 
-	function startTimer() {
-		timer = window.setInterval(() => {
-			setGameTime((prev) => prev + 1);
-		}, 1000);
+	function handleClick(e: React.MouseEvent<HTMLInputElement>) {
+		setGameChoice(e.currentTarget.id as gameChoice);
+	}
+
+	let description: string = 'An interactive Where is Waldo game based on the anime One Piece! ';
+	if (gameChoice === 'official') {
+		description =
+			"The Official Art from the One Piece and Where' waldo collab";
+	} else if (gameChoice === 'fanmade') {
+		description =
+			'A fan made art made by @JesseMartin featuring One Piece characters, youtubers and 5 pandamans';
 	}
 
 	return (
 		<div className={styles.Home}>
-
+			<h1 className={styles.title}>Where Is Wally?</h1>
+			<h2 className={styles.edition}>One Piece Edition!</h2>
+			<p className={styles.description}>{description}</p>
+			<div className={styles.buttons}>
+				<input
+					type='radio'
+					name='choice'
+					id='official'
+					onClick={handleClick}
+					className={styles.radio}
+				/>
+				<label
+					htmlFor='official'
+					className={styles.radioLabel}
+				>
+					Official
+				</label>
+				<input
+					type='radio'
+					name='choice'
+					id='fanmade'
+					onClick={handleClick}
+					className={styles.radio}
+				/>
+				<label
+					htmlFor='fanmade'
+					className={styles.radioLabel}
+				>
+					Fanmade
+				</label>
+				<button className={styles.button}>Play</button>
+			</div>
+			{gameChoice !== '' ? (
+				<div
+					className={styles.imageContainer}
+					style={{
+						backgroundImage: 'url(/images/' + gameChoice + '.png)',
+					}}
+				></div>
+			) : <div className={styles.placeholder}></div>}
 		</div>
 	);
 }
