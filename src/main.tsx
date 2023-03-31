61dc29326c67d4f92e6fa6a60cbbc50f6926f1a6
shayshahal <shay840@gmail.com>
2023-03-31 22:44:46 +0300

    firebase setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import {
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	getFirestore,
+} from 'firebase/firestore';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCo5C6p3p_HPnEE0joxH1gnIv-3XAxLoKY',
+	authDomain: 'where-s-waldo-68512.firebaseapp.com',
+	projectId: 'where-s-waldo-68512',
+	storageBucket: 'where-s-waldo-68512.appspot.com',
+	messagingSenderId: '16399107379',
+	appId: '1:16399107379:web:8c7691081cb7f37b5baa46',
+};
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+getDocs(collection(firestore, 'fanmade')).then((docSnap) => {
+	docSnap.forEach((doc) => {
+		console.log(doc.id, ' =>', doc.data());
+	});
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
