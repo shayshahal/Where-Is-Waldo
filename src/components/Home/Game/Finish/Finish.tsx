e0accd5981ac2057a75332b87c2ec8d9d03cd4ca
shayshahal <shay840@gmail.com>
2023-04-02 15:34:01 +0300

    add css classes to finish

--- src/components/Home/Game/Finish/Finish.tsx
+++ src/components/Home/Game/Finish/Finish.tsx
@@ -12,14 +12,14 @@ function Finish({ time }: propTypes) {
     }
 	return (
 		<div className={styles.Finish}>
-			<form method='post' onSubmit={handleSubmit} className={styles.container}>
-				<h1>Congratulations!</h1>
-				<h3>You finished in {toHHMMSS(time)}</h3>
+			<form method='post' onSubmit={handleSubmit} className={styles.form}>
+				<h1 className={styles.title}>Congratulations!</h1>
+				<h3 className={styles.time}>You finished in {toHHMMSS(time)}</h3>
 				<div className={styles.inputContainer}>
 					<label htmlFor='input' className={styles.label}>Enter your name here:</label>
 					<input type='text' id='input' className={styles.input}></input>
 				</div>
-                <button>To Leaderboards</button>
+                <button className={styles.button}>To Leaderboards</button>
 			</form>
 		</div>
 	);
